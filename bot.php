ccb7a303efb257dfed1a53ff810f69ac70368b06
pornmasa Namtirach <maysaa142@gmail.com>
2017-08-14 20:27:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,7 +11,7 @@
  	// Loop through each event
  	foreach ($events['events'] as $event) {
  		// Reply only when message sent is in 'text' format
- 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {+			// Get text sent
+ 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent
  			$text = $event['message']['text'];
  			// Get replyToken
  			$replyToken = $event['replyToken'];
@@ -32,10 +32,10 @@
  			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
  
  			$ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
  			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
  			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
- 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
  			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
  			$result = curl_exec($ch);
  			curl_close($ch);
