625a1dd00cb6911f90f433eb46b61964b56fd11f
pornmasa Namtirach <maysaa142@gmail.com>
2017-08-16 17:27:54 +0700

    $context = stream_context_create($opts);

--- bot.php
+++ bot.php
@@ -40,7 +40,8 @@ if (!is_null($events['events'])) {
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
+			$context = stream_context_create($opts);
+			
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
