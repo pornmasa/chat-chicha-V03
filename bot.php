a8af61d2d089b38b96540ee4b5bd7655fbc211fa
pornmasa Namtirach <maysaa142@gmail.com>
2017-08-16 17:38:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 		        $text=str_replace("",$pieces[0]);
-    			$mesages=str_replace("",$pieces[1]);
+    			$mesagesd=str_replace("",$pieces[1]);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 			$messages = [
 				 
 				'type' => 'text',
-				'text' => 'สวัสดีค่ะ'
+				'text' => '$mesagesd'
 				 
 				
 			];
